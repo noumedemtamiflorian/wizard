cbb40e2e8d63e2d5c6819bb7f66d71df33eac187
noumedem tami florian <noumedemtamiflorian@gmail.com>
2023-02-18 22:48:41 +0100

    Ajout des commentaires pour la boite Modal

--- src/components/ModalConfirm.js
+++ src/components/ModalConfirm.js
@@ -1,13 +1,20 @@
+// Importer React
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 
+// ModalConfirm est un composant qui affiche une boîte de dialogue de confirmation
 const ModalConfirm = ({ show, handleConfirm }) => {
   return (
+    // Déclaration d'une div contenant la modal et ses classes
     <div className={`modal ${show ? "d-block" : ""}`}>
+      {/*  Déclaration d'une div pour le dialogue de la modal */}
       <div className="modal-dialog" role="document">
+        {/*  Déclaration d'une div pour le contenu de la modal */}
         <div className="modal-content">
+          {/*  Déclaration d'une div pour l'en-tête de la modal */}
           <div className="modal-header">
+            {/*  Déclaration d'un titre pour la modal */}
             <h5 className="modal-title">Confirmation</h5>
+            {/*  Déclaration d'un bouton pour fermer la modal */}
             <button
               type="button"
               className="close"
@@ -15,11 +22,15 @@ const ModalConfirm = ({ show, handleConfirm }) => {
               aria-label="Close"
               onClick={handleConfirm}
             >
+              {/*  Déclaration d'un span pour le croix de fermeture de la modal */}
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
+          {/*  Déclaration d'une div pour le corps de la modal */}
           <div className="modal-body">Le message a été bien envoyé. </div>
+          {/*  Déclaration d'une div pour le pied de la modal */}
           <div className="modal-footer text-center">
+            {/* Déclaration d'un bouton pour confirmer l'envoi du message */}
             <button
               type="button"
               className="btn btn-primary"
